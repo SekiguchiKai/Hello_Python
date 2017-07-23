23a3bcbeb91c5f13d68ad9c0380c27f4a6382d91
sekky0905 <ks.sekiguchi.0905@gmail.com>
2017-07-23 20:53:01 +0900

    コンストラクタを追加

--- class.py
+++ class.py
@@ -14,3 +14,20 @@ python.name = "Python"
 python.type = "Dynamic"
 python.since = 1991
 print("{0}は{1}で{2}年からある".format(python.name, python.type, python.since))
+
+
+# コンストラクタ
+class Person:
+    # コンストラクタの書き方はこうらしい
+    # self = this
+    def __init__(self, name):
+        self.name = name
+
+sekky = Person("Sekky")
+print(sekky.name)
+
+# 以下は当然エラーになる
+# sekky0905 = Person()
+# print(sekky0905.name)
+
+
--- for.py
+++ for.py
@@ -1,4 +1,4 @@
-for i in range(1, 10):
+for i in range(1, 101):
     if i % 15 == 0:
         print("Fizz Buzz!")
     elif i % 3 == 0:
@@ -7,5 +7,3 @@ for i in range(1, 10):
         print("Buzz!")
     else:
         print(i)
-
-    i += 1
