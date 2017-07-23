872155840897b0fb7efe4288d708f205b7b1aa3d
sekky0905 <ks.sekiguchi.0905@gmail.com>
2017-07-23 20:31:28 +0900

    passを追加

--- def.py
+++ def.py
@@ -4,3 +4,20 @@ def calc_tax(value, tax_rate=0.08):
 calc_tax(100, 0.03)
 calc_tax(100, 0.05)
 calc_tax(100)
+
+
+# 関数を返値にできるよ
+def return_calc_tax(value, tax_rate=0.08):
+    return calc_tax(value, tax_rate)
+
+return_calc_tax(100, 0.03)
+return_calc_tax(100, 0.05)
+return_calc_tax(100)
+
+
+# 何もしないということをpassで現わせるらしい
+def return_pass():
+    pass
+
+print(return_pass())
+
