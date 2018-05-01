55b9034ca832595d307a34dde31c04659e5918db
sekky0905 <ks.sekiguchi.0905@gmail.com>
2018-05-01 10:02:14 +0900

    値を学んだ

---
+++ second/data.py
@@ -0,0 +1,28 @@
+# String
+s1 = "Python"
+print(s1)
+# \nは改行
+s2 = "A\nB\nC"
+print(s2)
+# \tはtab
+s3 = "A\tB\tC"
+print(s3)
+
+# 複数行にまたがる文章
+s4 = """
+I am learning Python, because I want to earn following skills.
+1. Machine Learning
+2. Data Analysis
+Python is necessary for the above skills.
+"""
+
+print(s4)
+
+# int
+i = 5
+
+# float
+f = 10.5
+
+# bool (大文字)
+b = True
