cb3e0f8dbce42814f16451e0785536ad899c156a
sekky0905 <ks.sekiguchi.0905@gmail.com>
2018-05-15 09:49:27 +0900

    メソッドを追加した

--- second/class_2.py
+++ second/class_2.py
@@ -9,12 +9,25 @@ class Person:
         # インスタンス変数
         self.name = name
 
+    # インスタンスメソッド
+    def say_hello(self):
+        print("hello,{0}".format(self.name))
+
+    # クラスメソッド
+    @classmethod
+    def show_num_of_instance(cls):
+        print("The number of instances are {0}".format(cls.count))
+
 
 
 sekky = Person("sekky")
 print(sekky.name)
 print(sekky.count)
+sekky.say_hello()
+sekky.show_num_of_instance()
 
 taro = Person("太郎")
 print(taro.name)
 print(taro.count)
+taro.say_hello()
+sekky.show_num_of_instance()
